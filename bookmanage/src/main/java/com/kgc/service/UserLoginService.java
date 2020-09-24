274dd37a086709d6a14003e627106b2f04cffe34
wangxinyu1008 <2401816735@qq.com>
2020-09-24 21:47:00 +0800

    王欣宇实现了登录

---
+++ bookmanage/src/main/java/com/kgc/service/UserLoginService.java
@@ -0,0 +1,11 @@
+package com.kgc.service;
+
+import com.kgc.pojo.User;
+
+/**
+ * @author shkstart
+ * @create 2020-09-24 21:25
+ */
+public interface UserLoginService {
+    User users(String name);
+}
---
+++ bookmanage/src/main/java/com/kgc/service/impl/UserLoginServiceImpl.java
@@ -0,0 +1,31 @@
+package com.kgc.service.impl;
+
+import com.kgc.mapper.UserMapper;
+import com.kgc.pojo.User;
+import com.kgc.pojo.UserExample;
+import com.kgc.service.UserLoginService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-09-24 21:26
+ */
+@Service("userLoginService")
+public class UserLoginServiceImpl implements UserLoginService {
+    @Resource
+    UserMapper userMapper;
+    @Override
+    public User users(String name) {
+        UserExample userExample=new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andNameEqualTo(name);
+        List<User> users = userMapper.selectByExample(userExample);
+        if(users.size()>0){
+            return users.get(0);
+        }
+        return null;
+    }
+}
